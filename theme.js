2c23a2cba04ee5f03c8229a059a416726a822f49
Ronakvarshney <152082317+Ronakvarshney@users.noreply.github.com>
2024-05-29 10:40:42 -0700

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+
+export const ThemeContext =createContext({
+    thememode:"light",
+    darktheme:()=>{},
+    lighttheme:()=>{},
+})
+
+
+export const ThemeProvider=ThemeContext.Provider
+
+export default function usetheme(){
+    return useContext(ThemeContext)
+}
